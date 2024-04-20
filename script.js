3f0dd1aa03aeb0317c85345b1188332512074cb6
drew <drew@drews-MacBook-Pro.local>
2024-04-20 00:41:24 -0700

    fixing smaller device spacing

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*!
+ * Webflow: Front-end site library
+ * @license MIT
+ * Inline scripts may access the api using an async handler:
+ *   var Webflow = Webflow || [];
+ *   Webflow.push(readyFunction);
+ */
+
 (() => {
     var JV = Object.create;
     var _a = Object.defineProperty;
